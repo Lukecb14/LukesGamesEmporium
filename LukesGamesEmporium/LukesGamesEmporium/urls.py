e597bf170866a464bfe5ef10d5c975e7e117168e
Lukecb14 <114123793+Lukecb14@users.noreply.github.com>
2024-12-15 22:48:07 -0500

    Create login and signup pages

--- LukesGamesEmporium/LukesGamesEmporium/urls.py
+++ LukesGamesEmporium/LukesGamesEmporium/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from lge.views import home
+from lge.views import home, signup
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('signup/', signup.signup, name='signup'),
     path('', home.home, name='lge-home'),
 ]
